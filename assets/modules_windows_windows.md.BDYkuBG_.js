ac3b37c32a009a0f5411274f65a5468851fc1bb1
dengshaomin86 <dengshaomin86@163.com>
2025-04-19 18:14:22 +0800

    docs: 自动更新 2025-04-19 18:14:16.27

---
+++ assets/modules_windows_windows.md.BDYkuBG_.js
@@ -0,0 +1,12 @@
+import{_ as s,c as a,o as n,a2 as e}from"./chunks/framework.Ps2l0eYJ.js";const m=JSON.parse('{"title":"windows","description":"","frontmatter":{},"headers":[],"relativePath":"modules/windows/windows.md","filePath":"modules/windows/windows.md"}'),t={name:"modules/windows/windows.md"},i=e(`<h1 id="windows" tabindex="-1">windows <a class="header-anchor" href="#windows" aria-label="Permalink to &quot;windows&quot;">​</a></h1><h2 id="_1-系统盘制作" tabindex="-1">1. 系统盘制作 <a class="header-anchor" href="#_1-系统盘制作" aria-label="Permalink to &quot;1. 系统盘制作&quot;">​</a></h2><ul><li><a href="https://www.ventoy.net/cn/index.html" target="_blank" rel="noreferrer">ventoy</a></li><li><a href="https://github.com/ventoy/Ventoy/releases" target="_blank" rel="noreferrer">ventoy releases</a></li></ul><h2 id="_2-重装系统" tabindex="-1">2. 重装系统 <a class="header-anchor" href="#_2-重装系统" aria-label="Permalink to &quot;2. 重装系统&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>1. 备份文件</span></span>
+<span class="line"><span>- C:\\Users\\Administrator\\.ssh</span></span>
+<span class="line"><span>- C:\\Users\\Administrator\\.vscode</span></span>
+<span class="line"><span>- C:\\Users\\Administrator\\.gitconfig</span></span>
+<span class="line"><span>- C:\\Users\\Administrator\\.npmrc</span></span>
+<span class="line"><span>- 备份个人文档</span></span>
+<span class="line"><span>- 备份浏览器书签</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>2. 插入 U 盘，重启电脑，按 F8 进入选择启动设备界面</span></span>
+<span class="line"><span>3. 选择 U 盘启动，进入 U 盘系统</span></span>
+<span class="line"><span>4. 安装系统</span></span>
+<span class="line"><span>5. 安装常用软件</span></span></code></pre></div>`,5),o=[i];function p(l,r,d,c,h,_){return n(),a("div",null,o)}const u=s(t,[["render",p]]);export{m as __pageData,u as default};
