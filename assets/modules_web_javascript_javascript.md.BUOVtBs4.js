3be05aef5d901309b2282d586f955c99ef94931b
dengshaomin86 <dengshaomin86@163.com>
2026-03-19 02:36:41 +0800

    docs: 自动更新 2026/03/19 周四  2:36:25.32

---
+++ assets/modules_web_javascript_javascript.md.BUOVtBs4.js
@@ -0,0 +1,17 @@
+import{_ as a,c as s,o as n,a4 as p}from"./chunks/framework.BX6rvgvq.js";const m=JSON.parse('{"title":"javascript","description":"","frontmatter":{},"headers":[],"relativePath":"modules/web/javascript/javascript.md","filePath":"modules/web/javascript/javascript.md","lastUpdated":1773858860000}'),e={name:"modules/web/javascript/javascript.md"},t=p(`<h1 id="javascript" tabindex="-1">javascript <a class="header-anchor" href="#javascript" aria-label="Permalink to &quot;javascript&quot;">​</a></h1><h2 id="js的数据类型" tabindex="-1">js的数据类型 <a class="header-anchor" href="#js的数据类型" aria-label="Permalink to &quot;js的数据类型&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>JavaScript 目前包含八种数据类型，其中可以分成两大类。</span></span>
+<span class="line"><span>基本数据类型（原始值）：</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Number（数值，包含NaN）</span></span>
+<span class="line"><span>String（字符串）</span></span>
+<span class="line"><span>Boolean（布尔值）</span></span>
+<span class="line"><span>Undefined（未定义/未初始化）</span></span>
+<span class="line"><span>Null（空对象）</span></span>
+<span class="line"><span>Symbol（独一无二的值，ES6 新增）</span></span>
+<span class="line"><span>BigInt （大整数，能够表示超过 Number 类型大小限制的整数，ES 2020新增）</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>引用数据类型（引用值）：</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>Object（对象。Array/数组 和 function/函数 也属于对象的一种）</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>基本数据类型的值是不可变的，你无法修改值本身，你只能给代表它的变量重新赋值，将原来的值覆盖。而引用数据类型是可变的——它们的值是可修改的。</span></span>
+<span class="line"><span>7种基本数据类型除了 Null 和 Undefined 之外，其他基本数据类型（String、Number、Boolean、Symbol、BigInt）都可以通过对象包装器（也称为包装对象）来创建对应的对象。这些包装对象提供了额外的方法和属性，使得基本数据类型可以像对象一样使用。</span></span></code></pre></div>`,3),l=[t];function i(c,r,o,d,_,h){return n(),s("div",null,l)}const v=a(e,[["render",i]]);export{m as __pageData,v as default};
