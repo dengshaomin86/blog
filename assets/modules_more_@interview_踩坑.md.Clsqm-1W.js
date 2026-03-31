2948aff9c76672d6ae5a6f6cb11d6c5ee5615d78
dengshaomin86 <dengshaomin86@163.com>
2026-04-01 01:23:23 +0800

    docs: 自动更新 2026/04/01 周三  1:22:58.15

---
+++ assets/modules_more_@interview_踩坑.md.Clsqm-1W.js
@@ -0,0 +1,11 @@
+import{_ as s,c as a,o as n,a6 as e}from"./chunks/framework.C2DR_BI-.js";const u=JSON.parse('{"title":"踩坑记录","description":"","frontmatter":{},"headers":[],"relativePath":"modules/more/@interview/踩坑.md","filePath":"modules/more/@interview/踩坑.md","lastUpdated":1774976743000}'),p={name:"modules/more/@interview/踩坑.md"},t=e(`<h1 id="踩坑记录" tabindex="-1">踩坑记录 <a class="header-anchor" href="#踩坑记录" aria-label="Permalink to &quot;踩坑记录&quot;">​</a></h1><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span>1. IE watch 失效</span></span>
+<span class="line"><span>2. 粘贴换行符后面的内容丢失，操作粘贴板内容层级深视图更新问题</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>3. 自动构建：Linux文件大小写敏感</span></span>
+<span class="line"><span>4. 读取数据：柯里化，闭包，保留对数据的引用。销毁，防止内存占用</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>5. .el-overlay z-index：遮罩zindex自增</span></span>
+<span class="line"><span>6. 窗口 store 同步：focus时更新同步</span></span>
+<span class="line"><span></span></span>
+<span class="line"><span>7. hash 路由参数改变不会更改 history.state, 写插件记录</span></span>
+<span class="line"><span>8. 微应用全局 css 样式污染，scoped、加作用域或前缀</span></span></code></pre></div>`,2),i=[t];function l(o,c,r,d,_,h){return n(),a("div",null,i)}const v=s(p,[["render",l]]);export{u as __pageData,v as default};
